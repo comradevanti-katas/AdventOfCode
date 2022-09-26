df61195966de5568755d49339eb2f49b26c4d427
ComradeVanti <kingvanti650@gmail.com>
2022-09-26 08:41:05 +0200

    feat(2015-1): parse strings

--- 2015/Day 1/Common.Tests/ParseTests.fs
+++ 2015/Day 1/Common.Tests/ParseTests.fs
@@ -7,12 +7,12 @@ open Swensen.Unquote.Assertions
 let ``( is parsed as Up`` () =
     let direction = Parse.single '('
     direction =! Some Up
-    
+
 [<Fact>]
 let ``) is parsed as Down`` () =
     let direction = Parse.single ')'
     direction =! Some Down
-    
+
 [<Theory>]
 [<InlineData('a')>]
 [<InlineData('/')>]
@@ -22,3 +22,13 @@ let ``) is parsed as Down`` () =
 let ``Characters other than ( and ) cannot be parsed`` c =
     let direction = Parse.single c
     direction =! None
+
+[<Fact>]
+let ``Sequences cannot be parsed if any character cannot be parsed`` () =
+    let directions = Parse.sequence "(a)"
+    directions =! None
+    
+[<Fact>]
+let ``Sequences can be parsed if all characters can be parsed`` () =
+    let directions = Parse.sequence "(())"
+    directions =! Some [Up; Up; Down; Down]
--- 2015/Day 1/Common/Parse.fs
+++ 2015/Day 1/Common/Parse.fs
@@ -5,3 +5,5 @@ let single c =
     if c = '(' then Some Up
     elif c = ')' then Some Down
     else None
+
+let sequence s = s |> Seq.map single |> Seq.toList |> Option.sequenceList
