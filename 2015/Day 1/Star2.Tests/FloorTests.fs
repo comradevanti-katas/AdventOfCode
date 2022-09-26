8be8777ed5ad5cde73ad3f9e3010b435193af04a
ComradeVanti <kingvanti650@gmail.com>
2022-09-26 10:40:22 +0200

    feat(2015-1-2): done

---
+++ 2015/Day 1/Star2.Tests/FloorTests.fs
@@ -0,0 +1,18 @@
+﻿module AdventOfCode.Y2015.Day1.Star2.FloorTests
+
+open AdventOfCode.Y2015.Day1
+open Xunit
+open Swensen.Unquote.Assertions
+
+[<Theory>]
+[<InlineData(")", 1)>]
+[<InlineData("()())", 5)>]
+[<InlineData("((())))", 7)>]
+let ``Correct instruction index found`` input expectedIndex =
+
+    let basementIndex =
+        input
+        |> Parse.sequence
+        |> Option.map Floor.findBasementIndexFor
+
+    basementIndex =! expectedIndex
---
+++ 2015/Day 1/Star2/Floor.fs
@@ -0,0 +1,13 @@
+﻿module AdventOfCode.Y2015.Day1.Star2.Floor
+
+open AdventOfCode.Y2015.Day1.Star1
+
+let findBasementIndexFor directions =
+    
+    let rec findBasement floor i =
+        let direction = directions |> List.item i
+        let newFloor = Floor.moveFrom floor direction
+        if newFloor = -1 then i + 1
+        else findBasement newFloor (i + 1)
+        
+    findBasement 0 0
