969f9516f5a023251ac31eb3634c334fe4967c38
ComradeVanti <kingvanti650@gmail.com>
2022-09-29 09:00:24 +0200

    feat(2015-2): calc box surface area

---
+++ 2015/Day2/Common.Tests/BoxTests.fs
@@ -0,0 +1,45 @@
+﻿module AdventOfCode.Y2015.Day2.BoxTests
+
+open Xunit
+open Swensen.Unquote.Assertions
+
+[<Theory>]
+[<InlineData(1, 1, 1, 1)>]
+[<InlineData(1, 2, 1, 2)>]
+[<InlineData(1, 1, 2, 1)>]
+[<InlineData(3, 2, 4, 6)>]
+let ``Box y-face area is length * width`` l w h expected =
+    let box = { Length = l; Width = w; Height = h }
+    let area = box |> Box.yFaceArea
+    area =! expected
+
+[<Theory>]
+[<InlineData(1, 1, 1, 1)>]
+[<InlineData(2, 1, 1, 2)>]
+[<InlineData(1, 2, 1, 1)>]
+[<InlineData(3, 2, 4, 12)>]
+let ``Box x-face area is length * height`` l w h expected =
+    let box = { Length = l; Width = w; Height = h }
+    let area = box |> Box.xFaceArea
+    area =! expected
+
+[<Theory>]
+[<InlineData(1, 1, 1, 1)>]
+[<InlineData(1, 2, 1, 2)>]
+[<InlineData(2, 1, 1, 1)>]
+[<InlineData(3, 2, 4, 8)>]
+let ``Box z-face area is width * height`` l w h expected =
+    let box = { Length = l; Width = w; Height = h }
+    let area = box |> Box.zFaceArea
+    area =! expected
+
+[<Theory>]
+[<InlineData(1, 1, 1, 6)>]
+[<InlineData(1, 1, 2, 10)>]
+[<InlineData(1, 2, 1, 10)>]
+[<InlineData(2, 1, 1, 10)>]
+[<InlineData(1, 2, 3, 22)>]
+let ``Box surface is all area summed times 2`` l w h expected =
+    let box = { Length = l; Width = w; Height = h }
+    let surface = box |> Box.surface
+    surface =! expected
---
+++ 2015/Day2/Common/Box.fs
@@ -0,0 +1,17 @@
+﻿[<Microsoft.FSharp.Core.RequireQualifiedAccess>]
+module AdventOfCode.Y2015.Day2.Box
+
+let yFaceArea box =
+    box.Length * box.Width
+    
+let xFaceArea box =
+    box.Length * box.Height
+    
+let zFaceArea box =
+    box.Width * box.Height
+    
+let surface box =
+    let xArea = box |> xFaceArea
+    let yArea = box |> yFaceArea
+    let zArea = box |> zFaceArea
+    (xArea + yArea + zArea) * 2
