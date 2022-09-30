1106f9a5ae8a6b7d2f1ef4830ab0ea3837330b9a
ComradeVanti <kingvanti650@gmail.com>
2022-09-30 15:07:31 +0200

    feat(2015-3-1): done

---
+++ 2015/Day 3/Star1.Tests/SleighTests.fs
@@ -0,0 +1,59 @@
+﻿module AdventOfCode.Y2015.Day3.Star1.SleighTests
+
+open System.Collections.Generic
+open FsCheck
+open FsCheck.Xunit
+open global.Xunit
+open Swensen.Unquote.Assertions
+open AdventOfCode.Y2015.Day3
+
+let private isAtLeast1 x = x >= 1
+
+let private is1 x = x = 1
+
+let private presentCount (kv: KeyValuePair<_, _>) = kv.Value
+
+let private house (kv: KeyValuePair<_, _>) = kv.Key
+
+
+[<Property>]
+let ``Total number of presents is direction-count + 1`` directions =
+    let directionCount = directions |> List.length
+    let presentsByHouse = Sleigh.follow directions
+    let totalPresents = presentsByHouse |> Seq.sumBy presentCount
+    totalPresents =! directionCount + 1
+
+[<Property>]
+let ``Visited houses get 1 or more presents`` directions =
+    let presentsByHouse = Sleigh.follow directions
+    presentsByHouse |> Seq.forall (presentCount >> isAtLeast1)
+
+[<Fact>]
+let ``Not moving delivers 1 present to the origin`` () =
+    let directions = []
+    let presentsByHouse = Sleigh.follow directions
+    let expected = Map.ofSeq [ (Sleigh.origin, 1) ]
+    presentsByHouse =! expected
+
+[<Property>]
+let ``Going in straight line visits each house once``
+    direction
+    (PositiveInt count)
+    =
+    let directions = List.replicate count direction
+    let presentsByHouse = Sleigh.follow directions
+    presentsByHouse |> Seq.forall (presentCount >> is1)
+
+[<Fact>]
+let ``Visiting a house for the first time gives 1 present`` () =
+    let directions = [ Up ]
+    let presentsByHouse = Sleigh.follow directions
+    let expected = Map.ofSeq [ (Sleigh.origin, 1); (0, 1), 1 ]
+    presentsByHouse =! expected
+
+[<Fact>]
+let ``Visiting a house repeatedly time gives 1 present each time`` () =
+    let directions = [ Up; Down ]
+    let presentsByHouse = Sleigh.follow directions
+    let expected = Map.ofSeq [ (Sleigh.origin, 2); (0, 1), 1 ]
+    presentsByHouse =! expected
---
+++ 2015/Day 3/Star1/Sleigh.fs
@@ -0,0 +1,36 @@
+﻿[<Microsoft.FSharp.Core.RequireQualifiedAccess>]
+module AdventOfCode.Y2015.Day3.Star1.Sleigh
+
+open AdventOfCode.Y2015.Day3
+
+let origin = (0, 0)
+
+let private deliverPresentTo house presentsPerHouse =
+    presentsPerHouse
+    |> Map.change house (function
+        | Some presentCount -> Some(presentCount + 1)
+        | None -> Some 1)
+
+let private move direction house =
+    let x, y = house
+
+    match direction with
+    | Up -> (x, y + 1)
+    | Right -> (x + 1, y)
+    | Down -> (x, y - 1)
+    | Left -> (x - 1, y)
+
+let rec private followFrom currentHouse directions presentsByHouse =
+    match directions with
+    | direction :: remaining ->
+        let nextHouse = currentHouse |> move direction
+
+        presentsByHouse
+        |> deliverPresentTo nextHouse
+        |> followFrom nextHouse remaining
+    | [] -> presentsByHouse
+
+let follow directions =
+    Map.empty
+    |> deliverPresentTo origin
+    |> followFrom origin directions
