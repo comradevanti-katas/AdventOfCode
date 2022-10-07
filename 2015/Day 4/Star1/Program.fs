0ada83553e5cbad159f7a2426fee89dc1af13f9f
ComradeVanti <kingvanti650@gmail.com>
2022-10-07 12:28:25 +0200

    feat(2015-4-1): done

--- 2015/Day 4/Star1/Program.fs
+++ 2015/Day 4/Star1/Program.fs
@@ -1,1 +1,36 @@
-﻿
+﻿open System.IO
+open AdventOfCode.Y2015.Day4.Star1.Hashing
+
+type ProgramError =
+    | NoPath
+    | IO of string
+    | Parse
+
+let tryGetArg index args =
+    args
+    |> Array.tryItem index
+    |> function
+        | Some arg -> Ok arg
+        | None -> Error NoPath
+
+let tryReadFileAt path =
+    try
+        File.ReadAllText path |> Ok
+    with
+    | _ -> Error(IO path)
+
+module Program =
+    [<EntryPoint>]
+    let main args =
+
+        args
+        |> tryGetArg 0
+        |> Result.bind tryReadFileAt
+        |> Result.map findNumberFor
+        |> function
+            | Ok number ->
+                printf $"The number for the correct hash is %d{number}"
+                0
+            | Error err ->
+                printf $"Error: %A{err}"
+                1
