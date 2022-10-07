a594e7090c729ef52f05a671978465313dc76a09
ComradeVanti <kingvanti650@gmail.com>
2022-10-07 11:45:23 +0200

    feat(2015-4-1): check hash validity

---
+++ 2015/Day 4/Star1.Tests/HashingTests.fs
@@ -0,0 +1,20 @@
+﻿module AdventOfCode.Y2015.Day4.Star1.HashingTests
+
+open Xunit
+open Swensen.Unquote.Assertions
+open AdventOfCode.Y2015.Day4.Star1.Hashing
+
+[<Theory>]
+[<InlineData("00000123")>]
+[<InlineData("00000abc")>]
+[<InlineData("000007846746876847687")>]
+[<InlineData("00000023jk35fdf42jkjj")>]
+let ``Hashes starting with 5 zeroes are valid`` hash =
+    hash |> isValidHash =! true
+
+[<Theory>]
+[<InlineData("0000123")>]
+[<InlineData("abc")>]
+[<InlineData("")>]
+let ``Hashes not starting with 5 zeroes are invalid`` hash =
+    hash |> isValidHash =! false
---
+++ 2015/Day 4/Star1/Hashing.fs
@@ -0,0 +1,7 @@
+﻿module AdventOfCode.Y2015.Day4.Star1.Hashing
+
+let isValidHash hash =
+    if hash |> String.length >= 5 then
+        hash |> Seq.take 5 |> Seq.forall ((=) '0')
+    else
+        false
