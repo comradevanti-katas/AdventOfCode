dfc1f5af10a7dd1b7f0d65994d2b6aebf887a4e9
ComradeVanti <kingvanti650@gmail.com>
2022-10-07 14:13:06 +0200

    feat(2015-5-1): check if string is nice

---
+++ 2015/Day 5/Star1.Tests/NaughtyNiceTests.fs
@@ -0,0 +1,40 @@
+﻿module AdventOfCode.Y2015.Day5.Star1.NaughtyNiceTests
+
+open Xunit
+open AdventOfCode.Y2015.Day5.Star1.NaughtyNice
+open Swensen.Unquote.Assertions
+
+let private shouldBeNice s = isNice s =! true
+
+let private shouldNotBeNice s = isNice s =! false
+
+[<Theory>]
+[<InlineData("aa")>]
+[<InlineData("aebb")>]
+[<InlineData("irre")>]
+let ``Nice strings must contain at least 3 vowels`` s = s |> shouldNotBeNice
+
+[<Theory>]
+[<InlineData("aio")>]
+[<InlineData("wowowai")>]
+[<InlineData("uiaro")>]
+let ``Nice strings must contain a letter twice in a row`` s =
+    s |> shouldNotBeNice
+
+[<Fact>]
+let ``Nice strings may not contain "ab"`` () = "abioo" |> shouldNotBeNice
+
+[<Fact>]
+let ``Nice strings may not contain "cd"`` () = "cdaioo" |> shouldNotBeNice
+
+[<Fact>]
+let ``Nice strings may not contain "pq"`` () = "pqaioo" |> shouldNotBeNice
+
+[<Fact>]
+let ``Nice strings may not contain "xy"`` () = "xyaioo" |> shouldNotBeNice
+
+[<Theory>]
+[<InlineData("ugknbfddgicrmopn")>]
+[<InlineData("aaiioo")>]
+[<InlineData("yeyayoo")>]
+let ``Strings which don't break any rules are nice`` s = s |> shouldBeNice
---
+++ 2015/Day 5/Star1/NaughtyNice.fs
@@ -0,0 +1,28 @@
+﻿module AdventOfCode.Y2015.Day5.Star1.NaughtyNice
+
+let private forbiddenStrings =
+    Set.ofList [ "ab"; "cd"; "pq"; "xy" ]
+
+let private vowels = Set.ofList [ "a"; "e"; "i"; "o"; "u" ]
+
+let private contains (sub: string) (s: string) = s.Contains sub
+
+let private containsForbiddenString s =
+    forbiddenStrings
+    |> Set.exists (fun forbidden -> s |> contains forbidden)
+
+let private containsLetterTwiceInARow s =
+    s |> Seq.pairwise |> Seq.exists (fun (a, b) -> a = b)
+
+let private vowelCount s =
+    vowels
+    |> Set.filter (fun vowel -> s |> contains vowel)
+    |> Set.count
+    
+let private contains3Vowels s =
+    s |> vowelCount >= 3
+
+let isNice s =
+    s |> (not << containsForbiddenString)
+    && s |> containsLetterTwiceInARow
+    && s |> contains3Vowels
