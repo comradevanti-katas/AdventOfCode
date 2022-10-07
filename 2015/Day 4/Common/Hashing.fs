9b387198e6eb4787e10b13f10568db1b4ce16151
ComradeVanti <kingvanti650@gmail.com>
2022-10-07 12:39:09 +0200

    refactor(2015-4): make logic common

---
+++ 2015/Day 4/Common/Hashing.fs
@@ -0,0 +1,10 @@
+﻿module AdventOfCode.Y2015.Day4.Hashing
+
+open System
+open System.Security.Cryptography
+open System.Text
+
+let hash (key: string) =
+    let bytes = Encoding.ASCII.GetBytes key
+    let hashed = MD5.HashData bytes
+    Convert.ToHexString hashed
--- 2015/Day 4/Star1/Hashing.fs
+++ 2015/Day 4/Star1/Hashing.fs
@@ -1,19 +1,12 @@
 ﻿module AdventOfCode.Y2015.Day4.Star1.Hashing
 
-open System
-open System.Security.Cryptography
-open System.Text
+open AdventOfCode.Y2015.Day4.Hashing
 
 let isValidHash hash =
     if hash |> String.length >= 5 then
         hash |> Seq.take 5 |> Seq.forall ((=) '0')
     else
         false
-
-let hash (key: string) =
-    let bytes = Encoding.ASCII.GetBytes key
-    let hashed = MD5.HashData bytes
-    Convert.ToHexString hashed
 
 let findNumberFor key =
     let rec findNumberStartingAt num =
