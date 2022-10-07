31ce5566cc33b1ba1e8502000a9f38934ca7cf8c
ComradeVanti <kingvanti650@gmail.com>
2022-10-07 12:41:56 +0200

    feat(2015-4-2): done

---
+++ 2015/Day 4/Star2.Tests/HashingTests.fs
@@ -0,0 +1,20 @@
+﻿module AdventOfCode.Y2015.Day4.Star2.HashingTests
+
+open Xunit
+open Swensen.Unquote.Assertions
+open AdventOfCode.Y2015.Day4.Star2.Hashing
+
+[<Theory>]
+[<InlineData("000000123")>]
+[<InlineData("000000abc")>]
+[<InlineData("0000007846746876847687")>]
+[<InlineData("000000023jk35fdf42jkjj")>]
+let ``Hashes starting with 6 zeroes are valid`` hash =
+    hash |> isValidHash =! true
+
+[<Theory>]
+[<InlineData("00000123")>]
+[<InlineData("abc")>]
+[<InlineData("")>]
+let ``Hashes not starting with 6 zeroes are invalid`` hash =
+    hash |> isValidHash =! false
---
+++ 2015/Day 4/Star2/Hashing.fs
@@ -0,0 +1,20 @@
+﻿module AdventOfCode.Y2015.Day4.Star2.Hashing
+
+open AdventOfCode.Y2015.Day4.Hashing
+
+let isValidHash hash =
+    if hash |> String.length >= 6 then
+        hash |> Seq.take 6 |> Seq.forall ((=) '0')
+    else
+        false
+
+let findNumberFor key =
+    let rec findNumberStartingAt num =
+        let fullKey = $"%s{key}%i{num}"
+
+        if fullKey |> hash |> isValidHash then
+            num
+        else
+            findNumberStartingAt (num + 1)
+
+    findNumberStartingAt 1
