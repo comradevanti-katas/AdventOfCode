c2285816add2cac8f46df6646890de89ff51c0e9
ComradeVanti <kingvanti650@gmail.com>
2022-10-14 11:54:53 +0200

    feat(2015-5-2): done

---
+++ 2015/Day 5/Star2/NaughtyNice.fs
@@ -0,0 +1,11 @@
+﻿module AdventOfCode.Y2015.Day5.Star2.NaughtyNice
+
+open System.Text.RegularExpressions
+
+
+let private isolatedPairsRegex = Regex @".*(.{2}).*\1.*"
+let private oneBetweenRegex = Regex @".*(.{1}).{1}\1.*"
+
+let isNice s =
+    s |> isolatedPairsRegex.IsMatch
+    && s |> oneBetweenRegex.IsMatch
--- 2015/Day 5/Star2/Program.fs
+++ 2015/Day 5/Star2/Program.fs
@@ -1,1 +1,35 @@
-﻿
+﻿open System.IO
+open AdventOfCode.Y2015.Day5.Star2.NaughtyNice
+
+type ProgramError =
+    | NoPath
+    | IO of string
+
+let tryGetArg index args =
+    args
+    |> Array.tryItem index
+    |> function
+        | Some arg -> Ok arg
+        | None -> Error NoPath
+
+let tryReadFileAt path =
+    try
+        File.ReadLines path |> seq |> Ok
+    with
+    | _ -> Error(IO path)
+
+module Program =
+    [<EntryPoint>]
+    let main args =
+
+        args
+        |> tryGetArg 0
+        |> Result.bind tryReadFileAt
+        |> Result.map (Seq.filter isNice >> Seq.length)
+        |> function
+            | Ok count ->
+                printf $"There are %d{count} nice strings"
+                0
+            | Error err ->
+                printf $"Error: %A{err}"
+                1
