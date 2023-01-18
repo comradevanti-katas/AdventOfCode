b6a454ed352b27f270798f57a76b3e35929bd4af
Ramon Brullo <kingvanti650@gmail.com>
2023-01-18 08:39:27 +0100

    feat(2015-12-1): done

--- 2015/Day 12/Star1/Program.fs
+++ 2015/Day 12/Star1/Program.fs
@@ -1,1 +1,16 @@
-﻿
+﻿open System.Text.RegularExpressions
+open AdventOfCode.AdventProgram
+
+let private numberRegex = Regex @"-?\d+"
+
+let private eval json =
+    let matches = numberRegex.Matches json
+    let numbers = matches |> Seq.map (fun m -> m.Value) |> Seq.map int
+    numbers |> Seq.sum
+
+let private makeMsg sum = $"The sum is %i{sum}"
+
+let private program = makeProgram (allText) (parseWith Some) eval makeMsg
+
+[<EntryPoint>]
+let main args = program args
