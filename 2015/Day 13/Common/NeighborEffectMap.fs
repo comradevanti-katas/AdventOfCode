7ad3972ab67e03ad1baf5e17c58e91732bc0c4c8
Ramon Brullo <kingvanti650@gmail.com>
2023-01-26 16:59:32 +0100

    feat(2015-13-2): done

--- 2015/Day 13/Common/NeighborEffectMap.fs
+++ 2015/Day 13/Common/NeighborEffectMap.fs
@@ -6,12 +6,18 @@ type NeighborEffectMap =
 [<RequireQualifiedAccess>]
 module NeighborEffectMap =
 
+    let private empty = NeighborEffectMap Map.empty
+
+    let add relation (NeighborEffectMap map) =
+        map
+        |> Map.add (relation.Person, relation.Neighbor) relation.Effect
+        |> NeighborEffectMap
+
     let makeFrom relations =
 
-        let addTo map relation =
-            map |> Map.add (relation.Person, relation.Neighbor) relation.Effect
+        let addTo map relation = map |> add relation
 
-        relations |> Seq.fold addTo Map.empty |> NeighborEffectMap
+        relations |> Seq.fold addTo empty
 
     let peopleIn (NeighborEffectMap map) =
         map |> Map.keys |> Seq.map fst |> Seq.distinct
--- 2015/Day 13/Star2/Program.fs
+++ 2015/Day 13/Star2/Program.fs
@@ -1,1 +1,31 @@
-﻿
+﻿open AdventOfCode.AdventProgram
+open AdventOfCode.Y2015.Day13
+
+let private addMe map =
+
+    let addRelationWithMe map person =
+        map
+        |> NeighborEffectMap.add
+            { Person = "Me"
+              Neighbor = person
+              Effect = 0 }
+        |> NeighborEffectMap.add
+            { Person = person
+              Neighbor = "Me"
+              Effect = 0 }
+
+    let people = NeighborEffectMap.peopleIn map
+    (map, people) ||> Seq.fold addRelationWithMe
+
+let private eval relations =
+    let map = NeighborEffectMap.makeFrom relations |> addMe
+    SeatingArrangement.calcMaximumHappinessFor map
+
+let private makeMsg happiness =
+    $"The maximum happiness is %i{happiness}"
+
+let private program =
+    makeProgram allLines (parseEachWith Parse.tryRelation) eval makeMsg
+
+[<EntryPoint>]
+let main args = program args
