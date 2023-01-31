a9191816d0c90a93917ca625c665aa032f56ab87
ComradeVanti <kingvanti650@gmail.com>
2023-01-31 09:18:21 +0100

    refactor(2015-7): restart with new project template

--- 2015/Day 7/Common/AdventProgram.fs
+++ 2015/Day 7/Common/AdventProgram.fs
@@ -1,10 +1,9 @@
-﻿[<Microsoft.FSharp.Core.RequireQualifiedAccess>]
-module AdventOfCode.AdventProgram
+﻿module AdventOfCode.AdventProgram
 
 open System.IO
 open Microsoft.FSharp.Core
 
-type ConsoleApp = string [] -> int
+type ConsoleApp = string[] -> int
 
 type Error =
     | NoPath
@@ -18,19 +17,19 @@ let private tryGetArg index args =
         | Some arg -> Ok arg
         | None -> Error NoPath
 
-let tryReadTextAt path =
+let private tryReadTextAt path =
     try
         File.ReadAllText path |> Ok
-    with
-    | _ -> Error(IO path)
+    with _ ->
+        Error(IO path)
 
-let tryReadLinesAt path =
+let private tryReadLinesAt path =
     try
         File.ReadLines path |> Seq.toList |> Ok
-    with
-    | _ -> Error(IO path)
+    with _ ->
+        Error(IO path)
 
-let parseEach f =
+let parseEachWith f =
     (fun input ->
         input
         |> List.map f
@@ -39,15 +38,19 @@ let parseEach f =
             | Some instructions -> Ok instructions
             | None -> Error Parse)
 
-let parseAll f =
+let parseWith f =
     (fun input ->
         f input
         |> function
             | Some instructions -> Ok instructions
             | None -> Error Parse)
 
-let make read parse eval makeMsg : ConsoleApp =
-    (fun (args: string []) ->
+let allText = tryReadTextAt
+
+let allLines = tryReadLinesAt
+
+let makeProgram read parse eval makeMsg : ConsoleApp =
+    (fun (args: string[]) ->
         let path = args |> tryGetArg 0
         let content = path |> Result.bind read
         let parsed = content |> Result.bind parse
@@ -55,7 +58,7 @@ let make read parse eval makeMsg : ConsoleApp =
 
         match result with
         | Ok value ->
-            printf (makeMsg value)
+            printf $"%s{makeMsg value}"
             0
         | Error err ->
             printf $"Error: %A{err}"
--- 2015/Day 7/Star1/Program.fs
+++ 2015/Day 7/Star1/Program.fs
@@ -1,1 +1,16 @@
-﻿
+﻿module AdventOfCode.Y2015.Day7.Star1.Program
+
+open AdventOfCode.AdventProgram
+
+let private parse _ = failwith "Implement"
+
+let private read _ = failwith "Implement"
+
+let private eval _ = failwith "Implement"
+
+let private makeMsg _ = failwith "Implement"
+
+let private program = makeProgram read parse eval makeMsg
+
+[<EntryPoint>]
+let main args = program args
