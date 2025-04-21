3db993d68c110bc8d1ae14585b6e2b2b4a7a12a8
Ramon Brullo <kingvanti650@gmail.com>
2025-04-21 18:20:15 +0200

    update template

--- 2016/template/src/index.ts
+++ 2016/template/src/index.ts
@@ -2,11 +2,20 @@ import { Effect, pipe, String } from 'effect';
 import { EOL } from 'os';
 import { readInput } from './aocUtils';
 
-const program = pipe(
-    readInput(),
-    Effect.map(String.split(EOL)),
+const prepareInput = pipe(readInput(), Effect.map(String.split(EOL)));
+
+const star1Program = pipe(
+    prepareInput,
     Effect.map((lines) => lines.length),
     Effect.map((count) => `There are ${count} lines in the input.`)
 );
 
-Effect.runPromise(program).then(console.log).catch(console.error);
+Effect.runPromise(star1Program).then(console.log).catch(console.error);
+
+const star2Program = pipe(
+    prepareInput,
+    Effect.map((lines) => lines.length),
+    Effect.map((count) => `There are ${count} lines in the input.`)
+);
+
+Effect.runPromise(star2Program).then(console.log).catch(console.error);
