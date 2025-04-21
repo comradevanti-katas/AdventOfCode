fff28ba8bb32e47c8a792f26396d996db5a988ef
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2025-04-21 17:25:13 +0200

    implement final calc

--- 2016/day-1/src/domain.ts
+++ 2016/day-1/src/domain.ts
@@ -3,19 +3,76 @@ export enum Dir {
     Left,
 }
 
+export enum LookDir {
+    North,
+    East,
+    South,
+    West,
+}
+
 export interface Instruction {
     readonly dir: Dir;
     readonly dist: number;
 }
 
 export const goRight = (n: number) =>
-    ({ dir: Dir.Right, dist: n } satisfies Instruction);
+    ({ dir: Dir.Right, dist: n }) satisfies Instruction;
 
 export const goLeft = (n: number) =>
-    ({ dir: Dir.Left, dist: n } satisfies Instruction);
+    ({ dir: Dir.Left, dist: n }) satisfies Instruction;
+
+interface Position {
+    x: number;
+    y: number;
+    lookDirection: LookDir;
+}
 
 export const calcShortestDistance = (
     instructions: ReadonlyArray<Instruction>
 ) => {
-    return instructions.reduce((previousValue, b) => previousValue + b.dist, 0);
+    const startPos: Position = { x: 0, y: 0, lookDirection: LookDir.North };
+    const finalPos = instructions.reduce(calcNextPosition, startPos);
+    return finalPos.x + finalPos.y;
+};
+
+const calcNextPosition = (prev: Position, movement: Instruction): Position => {
+    switch (prev.lookDirection) {
+        case LookDir.North:
+            return {
+                x:
+                    prev.x +
+                    movement.dist * (movement.dir === Dir.Left ? -1 : 1),
+                y: prev.y,
+                lookDirection:
+                    movement.dir === Dir.Left ? LookDir.West : LookDir.East,
+            };
+
+        case LookDir.East:
+            return {
+                x: prev.x,
+                y:
+                    prev.y +
+                    movement.dist * (movement.dir === Dir.Left ? 1 : -1),
+                lookDirection:
+                    movement.dir === Dir.Left ? LookDir.North : LookDir.South,
+            };
+        case LookDir.South:
+            return {
+                x:
+                    prev.x +
+                    movement.dist * (movement.dir === Dir.Left ? 1 : -1),
+                y: prev.y,
+                lookDirection:
+                    movement.dir === Dir.Left ? LookDir.East : LookDir.West,
+            };
+        case LookDir.West:
+            return {
+                x: prev.x,
+                y:
+                    prev.y +
+                    movement.dist * (movement.dir === Dir.Left ? -1 : 1),
+                lookDirection:
+                    movement.dir === Dir.Left ? LookDir.South : LookDir.North,
+            };
+    }
 };
