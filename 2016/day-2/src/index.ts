abc115865ce77e8f23ba97020b09199b418eb2bb
Ramon Brullo <kingvanti650@gmail.com>
2025-04-21 19:24:12 +0200

    use solution in index

--- 2016/day-2/src/index.ts
+++ 2016/day-2/src/index.ts
@@ -1,12 +1,12 @@
 import { Effect, pipe, String } from 'effect';
 import { EOL } from 'os';
 import { readInput } from './aocUtils';
+import { solveBathroomCode } from './domain';
 
 const star1Program = pipe(
     readInput(),
-    Effect.map(String.split(EOL)),
-    Effect.map((lines) => lines.length),
-    Effect.map((count) => `There are ${count} lines in the input.`)
+    Effect.map(solveBathroomCode),
+    Effect.map((code) => `The code is "${code}"`)
 );
 
 Effect.runPromise(star1Program).then(console.log).catch(console.error);
