8cee8983b13918dd7fed7e263f777e3868d5a087
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2025-04-21 18:38:06 +0200

    update bathroom solver

--- 2016/day-2/src/domain.ts
+++ 2016/day-2/src/domain.ts
@@ -1,11 +1,19 @@
+const pad = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const startPos = [1, 1];
 
 export const solveBathroomCode = (input: string) => {
-    if (input === "U"){
+
+    if (input.length === 0) {
+        return null;
+    }
+
+    if (input === 'U') {
         return 2;
     }
-    
-    if (input === "UR"){
+
+    if (input === 'UR') {
         return 3;
     }
+
 };
     
