b662d66c1a448e5905ede508ba615a4189fbe93f
Ramon Brullo <kingvanti650@gmail.com>
2025-04-21 16:43:32 +0200

    add test

--- 2016/day-1/src/domain.spec.ts
+++ 2016/day-1/src/domain.spec.ts
@@ -1,8 +1,18 @@
+import fc from 'fast-check';
 import { expect, it } from 'vitest';
-import { calcShortestDistance } from './domain';
+import { calcShortestDistance, Dir, type Instruction } from './domain';
 
 it('should be zero for empty instructions', () => {
     const distance = calcShortestDistance([]);
 
     expect(distance).to.equal(0);
 });
+
+it('should be the number if we just walk in a straight line', () =>
+    fc.assert(
+        fc.property(fc.integer(), (dist) => {
+            const instruction = { dir: Dir.Left, dist } satisfies Instruction;
+            const actual = calcShortestDistance([instruction]);
+            expect(actual).to.equal(dist);
+        })
+    ));
