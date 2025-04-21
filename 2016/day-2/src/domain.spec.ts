b3c06cbb7d4b3db4177b576b8ea83b0842f08fcf
Ramon Brullo <kingvanti650@gmail.com>
2025-04-21 18:37:55 +0200

    add test

--- 2016/day-2/src/domain.spec.ts
+++ 2016/day-2/src/domain.spec.ts
@@ -1,3 +1,5 @@
+import fc from 'fast-check';
+import { EOL } from 'os';
 import { expect, it } from 'vitest';
 import { solveBathroomCode } from './domain';
 
@@ -16,3 +18,14 @@ it('should give result for single corner input', () => {
     expect(password).to.equal(3);
 });
 
+it('password length matches line count', () =>
+    fc.assert(
+        fc.property(fc.integer({ min: 1, max: 20 }), (count) => {
+            let input = Array.from({ length: count })
+                .map(() => 'U')
+                .join(EOL);
+            let password = solveBathroomCode(input);
+
+            expect(password.toString()).to.have.length(count);
+        })
+    ));
