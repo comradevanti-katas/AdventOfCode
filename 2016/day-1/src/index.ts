5e0b605796e6b3c9b11ce34a8034f6aaaa1a14f1
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2025-04-21 16:42:00 +0200

    add pipeline

--- 2016/day-1/src/index.ts
+++ 2016/day-1/src/index.ts
@@ -1,11 +1,13 @@
-import {Effect, pipe, String} from 'effect';
+import {Effect, pipe, String, Array} from 'effect';
 import {readInput} from './aocUtils';
+import {parseInstruction} from "./parse";
+import {calcShortestDistance} from "./domain";
 
 const program = pipe(
   readInput(),
   Effect.map(String.split(", ")),
-  Effect.map((lines) => lines.length),
-  Effect.map((count) => `There are ${count} lines in the input.`)
+  Effect.map(Array.map(parseInstruction)),
+  Effect.map((instructionArray) => calcShortestDistance(instructionArray))
 );
 
 Effect.runPromise(program).then(console.log).catch(console.error);
