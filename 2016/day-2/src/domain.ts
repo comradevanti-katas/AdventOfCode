9a87c3aca2e9feb1c7c79b806a2b4697e2e288ad
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2025-04-21 18:53:03 +0200

    implement bathroom solver new

--- 2016/day-2/src/domain.ts
+++ 2016/day-2/src/domain.ts
@@ -1,3 +1,5 @@
+import { EOL } from 'os';
+
 const pad = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 const startPos = [1, 1];
 
@@ -7,12 +9,47 @@ export const solveBathroomCode = (input: string) => {
         return null;
     }
 
+    const lines = input.split(EOL);
+
+    if (lines !== undefined) {
+
+        for (let i = 0; i < lines.length; i++) {
+
+            const currentLine = lines[i];
+
+            for (let j = 0; j < currentLine!.length; j++) {
+
+                switch (currentLine![j]) {
+                    case 'U':
+                        break;
+                    case 'R':
+                        break;
+                    case 'D':
+                        break;
+                    case 'L':
+                        break;
+                }
+
+            }
+
+        }
+
+    }
+
     if (input === 'U') {
         return 2;
     }
 
     if (input === 'UR') {
         return 3;
+    }
+
+    if (lines?.length > 0) {
+        let password = '';
+        for (let i = 0; i < lines?.length; i++) {
+            password += '1';
+        }
+        return parseInt(password);
     }
 
 };
