f521caedfa9e3e22e5991c6d9b56a6d47ac11da6
Ramon Brullo <kingvanti650@gmail.com>
2025-04-21 18:29:40 +0200

    add test

--- 2016/day-2/src/domain.spec.ts
+++ 2016/day-2/src/domain.spec.ts
@@ -5,3 +5,9 @@ it('should give result for single input', () => {
     let password = solveBathroomCode('U');
     expect(password).to.equal(2);
 });
+
+it('should give result for single corner input', () => {
+    let password = solveBathroomCode('UR');
+    expect(password).to.equal(3);
+});
+
