8cfbda28856d28d090649da25491dd3f0192eec7
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2025-04-21 19:09:56 +0200

    implement bathroom solver pw

--- 2016/day-2/src/domain.ts
+++ 2016/day-2/src/domain.ts
@@ -1,39 +1,12 @@
 import { EOL } from 'os';
 
 const pad = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-const startPos = [1, 1];
+const startPos: [number,  number] = [1, 1];
 
 export const solveBathroomCode = (input: string) => {
 
     if (input.length === 0) {
         return null;
-    }
-
-    const lines = input.split(EOL);
-
-    if (lines !== undefined) {
-
-        for (let i = 0; i < lines.length; i++) {
-
-            const currentLine = lines[i];
-
-            for (let j = 0; j < currentLine!.length; j++) {
-
-                switch (currentLine![j]) {
-                    case 'U':
-                        break;
-                    case 'R':
-                        break;
-                    case 'D':
-                        break;
-                    case 'L':
-                        break;
-                }
-
-            }
-
-        }
-
     }
 
     if (input === 'U') {
@@ -44,6 +17,45 @@ export const solveBathroomCode = (input: string) => {
         return 3;
     }
 
+    const lines = input.split(EOL);
+    let password = "";
+    let currentPos: [number,  number] = startPos;
+    
+    if (lines !== undefined) {
+
+        for (let i = 0; i < lines.length; i++) {
+
+            const currentLine = lines[i];
+            
+            for (let j = 0; j < currentLine!.length; j++) {
+
+                switch (currentLine![j]) {
+                    case 'U':
+                        currentPos = [currentPos[0], currentPos[1] !== 2 ? currentPos[1] + 1 : currentPos[1]];
+                        break;
+                    case 'R':
+                        currentPos = [currentPos[0] !== 2 ? currentPos[0] + 1 : currentPos[0], currentPos[1] ];
+                        break;
+                    case 'D':
+                        currentPos = [currentPos[0], currentPos[1] !== 0 ? currentPos[1] - 1 : currentPos[1]];
+                        break;
+                    case 'L':
+                        currentPos = [currentPos[0] !== 0 ? currentPos[0] - 1 : currentPos[0], currentPos[1] ];
+                        break;
+                }
+
+            }
+            
+            password += pad[currentPos[1]]![currentPos[0]];
+
+        }
+        
+        return parseInt(password);
+
+    }
+
+  
+/*
     if (lines?.length > 0) {
         let password = '';
         for (let i = 0; i < lines?.length; i++) {
@@ -51,6 +63,10 @@ export const solveBathroomCode = (input: string) => {
         }
         return parseInt(password);
     }
+    
+ */
+    
+    
 
 };
     
